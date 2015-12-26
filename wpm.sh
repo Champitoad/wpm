13f25df7dca20bb31b9f139e4fad45d44417f149
Pablo Donato <pablo.donato@mailoo.org>
2015-12-26 15:26:14 +0100

    Replaced deprecated backticks by $() syntax

--- wpm.sh
+++ wpm.sh
@@ -7,7 +7,7 @@
 # > -Mc option to use the color passed in argument as a wallpaper
 # > -h help option
 
-SD=`dirname \`realpath $0\`` # This script directory
+SD=$(dirname $(realpath $0)) # This script directory
 
 source $SD/.wpm/cmd.sh # Load wallpaper get/set functions
 
@@ -37,14 +37,14 @@ checkmode () {
     if [[ ! $@ =~ $mode ]]; then
         local modeopts="-["
         for mode in $@; do
-            modeopts=$modeopts`echo $mode | head -c1 | sed "s/\(.\)/\U\1/"`"|"
+            modeopts=$modeopts$(echo $mode | head -c1 | sed "s/\(.\)/\U\1/")"|"
         done
         modeopts=${modeopts%?}"]"
         if [ $# -eq 1 ]; then
-            modeopts=`echo $modeopts | tr -d []`
+            modeopts=$(echo $modeopts | tr -d [])
         fi
         echo "Option -$option must be used with $modeopts option"
-        exit `printf "%d" "'$option"`
+        exit $(printf "%d" "'$option")
     fi
 }
 
@@ -53,21 +53,21 @@ checkmode () {
 
 # Get the line number of a wallpaper in a list
 get_numline () {
-    echo `grep -n "$1" $2 | cut -d: -f1`
+    echo $(grep -n "$1" $2 | cut -d: -f1)
 }
 # Load a list
 load_wpl () {
-    wps=`cat $1`
+    wps=$(cat $1)
 }
 # Save a wallpaper in a list
 save_wp () {
-    if [ -z "`grep \"$1\" $2`" ]; then
+    if [ -z "$(grep "$1" $2)" ]; then
         echo "$1" >> $2
     fi
 }
 # Remove a wallpaper from a list
 remove_wp () {
-    local numline=`get_numline "$1" $2`
+    local numline=$(get_numline "$1" $2)
     if [ "$numline" ]; then
         sed -i "${numline}d" $2
     else
@@ -125,14 +125,14 @@ while getopts ":f:d:s:r:p:n:RD:" opt; do
         # Remove current wallpaper from list
         r)
             checkmode $opt custom
-            remove_wp "`get_wp`" $OPTARG
+            remove_wp "$(get_wp)" $OPTARG
             exit 0
             ;;
     ### Last mode ###
         p|n)
             checkmode $opt last
-            numline=`get_numline "\`get_wp\`" $LAST_WPL`
-            nblines=`wc -l $LAST_WPL | cut -d\  -f1`
+            numline=$(get_numline "$(get_wp)" $LAST_WPL)
+            nblines=$(wc -l $LAST_WPL | cut -d\  -f1)
             case $opt in
         # Load $OPTARGth previous wallpaper in last list
             p)
@@ -149,7 +149,7 @@ while getopts ":f:d:s:r:p:n:RD:" opt; do
                 fi
                 ;;
             esac
-            set_wp "`sed \"${numline}q;d\" $LAST_WPL`"
+            set_wp "$(sed "${numline}q;d" $LAST_WPL)"
             exit 0
             ;;
     ### Multi mode ###
@@ -158,7 +158,7 @@ while getopts ":f:d:s:r:p:n:RD:" opt; do
             case $mode in
         # Append current wallpaper to custom list
             custom)
-                save_wp "`get_wp`" $OPTARG
+                save_wp "$(get_wp)" $OPTARG
                 ;;
         # Save last list in the file passed in argument
             last)
@@ -190,14 +190,14 @@ while getopts ":f:d:s:r:p:n:RD:" opt; do
             # Use default custom list in custom mode
             r)  
                 checkmode $OPTARG custom
-                remove_wp "`get_wp`" $CUSTOM_WPL
+                remove_wp "$(get_wp)" $CUSTOM_WPL
                 exit 0
                 ;;
         ### Last mode ###
             p|n)
                 checkmode $OPTARG last
-                numline=`get_numline "\`get_wp\`" $LAST_WPL`
-                nblines=`wc -l $LAST_WPL | cut -d\  -f1`
+                numline=$(get_numline "$(get_wp)" $LAST_WPL)
+                nblines=$(wc -l $LAST_WPL | cut -d\  -f1)
                 case $OPTARG in
             # Load previous wallpaper in last list
                 p)
@@ -214,7 +214,7 @@ while getopts ":f:d:s:r:p:n:RD:" opt; do
                     fi
                     ;;
                 esac
-                set_wp "`sed \"${numline}q;d\" $LAST_WPL`"
+                set_wp "$(sed "${numline}q;d" $LAST_WPL)"
                 exit 0
                 ;;
         ### Multi mode ###
@@ -236,11 +236,11 @@ while getopts ":f:d:s:r:p:n:RD:" opt; do
                     case $OPTARG in
             # Print current wallpaper path
                     f)
-                        echo "`get_wp`"
+                        echo "$(get_wp)"
                         ;;
             # Print current wallpaper directory path
                     d)
-                        echo "`dirname \"\`get_wp\`\"`"
+                        echo "$(dirname "$(get_wp)")"
                         ;;
                     esac
                     ;;
@@ -252,7 +252,7 @@ while getopts ":f:d:s:r:p:n:RD:" opt; do
                 case $mode in
             # Use default custom list in custom mode
                     custom)
-                        save_wp "`get_wp`" $CUSTOM_WPL
+                        save_wp "$(get_wp)" $CUSTOM_WPL
                         ;;
             # Replace default custom list by last list 
                     last)
@@ -315,8 +315,8 @@ fi
 
 ##### One instance of wpm at a time #####
 
-pids=`ps -C wpm -o pid=`
-if [ `echo "$pids" | wc -l | cut -d\  -f1` -gt 1 ]; then
+pids=$(ps -C wpm -o pid=)
+if [ $(echo "$pids" | wc -l | cut -d\  -f1) -gt 1 ]; then
     while read -r pid; do
         if [ $pid != $$ ]; then
             kill $pid
@@ -329,12 +329,12 @@ fi
 
 # Add wallpapers from the list of dirs
 for dir in "${dirs[@]}"; do
-    wps+=`find "$dir" -type f`"\n"
+    wps+=$(find "$dir" -type f)"\n"
 done
 wps=${wps%\\n} # Remove last \n
 
 # Exit if there is one or no wallpaper in the list
-wpsnb=`echo "$wps" | wc -l`
+wpsnb=$(echo "$wps" | wc -l)
 if [ $wpsnb -le 1 ]; then
     if [ $wpsnb -eq 0 ]; then
         echo "No wallpaper found. Exiting..."
@@ -352,11 +352,11 @@ echo "$wps" > $LAST_WPL
 while [ 1 ]; do
     # Randomize list if -R option enabled
     if [ $random = true ]; then
-        wps=`echo "$wps" | shuf`
+        wps=$(echo "$wps" | shuf)
     fi
     # Read the list line by line
     while read -r wp; do
-        if [ "$wp" != "`get_wp`" ] || [ $random = false ]; then
+        if [ "$wp" != "$(get_wp)" ] || [ $random = false ]; then
             # Change wallpaper (finally!)
             set_wp "$wp"
             # Exit if in normal mode...
